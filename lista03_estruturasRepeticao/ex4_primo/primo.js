4a0ee62b93173945465ad8ad3ad893f56c2c87a2
Jonathan Oliveira Bergamo <jonathanbergamo16@gmail.com>
2023-02-16 19:01:56 -0300

    1

--- lista03_estruturasRepeticao/ex4_primo/primo.js
+++ lista03_estruturasRepeticao/ex4_primo/primo.js
@@ -5,5 +5,12 @@ btnCalcular.addEventListener("click", calcular);
 function calcular() {
   let n1 = Number(document.querySelector("#n1").value);
 
-
+  let divisor = 2;
+  while(n1%divisor != 0)
+      divisor++;
+  
+  if(n1 == divisor)
+      console.log("Número Primo!");
+  else
+      console.log("Número NÃO primo!");
 }
